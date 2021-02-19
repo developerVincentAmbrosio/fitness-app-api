3e24650342ac2fbacc44910e9a9b2adc8e87a8cc
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-02-18 20:45:39 -0500

    created table and seed sql files

---
+++ sql-scripts/seed.user.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (user_id, first_name, last_name, email, user_pass, is_active)
+VALUES
+(1,'Raymond','Stantz','ray_stantz@msn.com','ecto',1),
+(2,'Peter','Venkman','drpete@yahoo.com','werty',1),
+(3,'Egon','Spengler','espengler@earthlink.com','cobalt23',0),
+(4,'Winston','Zeddemore','wzed123@hotmail.com','turnuwhite',1),
+(5,'Louis','Tully','louistully84@gmail.com','deathandtaxes',1),
+(6,'Janine','Melnitz','janinem@att.net','cosmo',1);
---
+++ sql-scripts/seed.workout.sql
@@ -0,0 +1,8 @@
+INSERT INTO workout (workout_id, user_id, exercise_id, set_number, num_of_reps, weight_used, date_completed, is_active)
+VALUES
+(1,2,3,1,15,70'',1),
+(1,2,3,2,12,80'',1),
+(1,2,3,3,8,90'',1),
+(2,4,1,1,15,80'',1),
+(2,4,1,2,12,90'',1),
+(2,4,1,3,8,100'',1);
