b0525fad221d0147dda6e22631bdb173493c35ed
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-02-20 14:53:48 -0500

    debugged server file

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 const pg = require('pg');
 
-cosnt db = knex({
+const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
 })
