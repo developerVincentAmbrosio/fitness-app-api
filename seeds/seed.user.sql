9cc70b326cc3acc1683b8cf97eed1dc30c9d9dbd
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-02-20 14:46:56 -0500

    debugged sql files, made tables, seeded

--- seeds/seed.user.sql
+++ seeds/seed.user.sql
@@ -1,8 +1,8 @@
-INSERT INTO user (user_id, first_name, last_name, email, user_pass, is_active)
+INSERT INTO fitness_user (id, first_name, last_name, email, user_pass, is_active)
 VALUES
-(1,'Raymond','Stantz','ray_stantz@msn.com','ecto',1),
-(2,'Peter','Venkman','drpete@yahoo.com','werty',1),
-(3,'Egon','Spengler','espengler@earthlink.com','cobalt23',0),
-(4,'Winston','Zeddemore','wzed123@hotmail.com','turnuwhite',1),
-(5,'Louis','Tully','louistully84@gmail.com','deathandtaxes',1),
-(6,'Janine','Melnitz','janinem@att.net','cosmo',1);
+(1,'Raymond','Stantz','ray_stantz@msn.com','ecto',true),
+(2,'Peter','Venkman','drpete@yahoo.com','werty',true),
+(3,'Egon','Spengler','espengler@earthlink.com','cobalt23',false),
+(4,'Winston','Zeddemore','wzed123@hotmail.com','turnuwhite',true),
+(5,'Louis','Tully','louistully84@gmail.com','deathandtaxes',true),
+(6,'Janine','Melnitz','janinem@att.net','cosmo',true);
--- seeds/seed.workout.sql
+++ seeds/seed.workout.sql
@@ -1,8 +1,8 @@
-INSERT INTO workout (workout_id, user_id, exercise_id, set_number, num_of_reps, weight_used, date_completed, is_active)
+INSERT INTO fitness_workout (id, use_id, ex_id, set_number, num_of_reps, weight_used, date_completed, is_active)
 VALUES
-(1,2,3,1,15,70'',1),
-(1,2,3,2,12,80'',1),
-(1,2,3,3,8,90'',1),
-(2,4,1,1,15,80'',1),
-(2,4,1,2,12,90'',1),
-(2,4,1,3,8,100'',1);
+(1,2,3,1,15,70,'02-20-21',true),
+(2,2,3,2,12,80,'02-20-21',true),
+(3,2,3,3,8,90,'02-20-21',true),
+(4,4,1,1,15,80,'02-20-21',true),
+(5,4,1,2,12,90,'02-20-21',true),
+(6,4,1,3,8,100,'02-20-21',true);
