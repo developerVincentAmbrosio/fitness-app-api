e96f414fb8a49e7ffb856cdf10f53bd694b44f5b
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-03-10 21:04:42 -0500

    console log

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") require('dotenv').config();
-
+console.log(process.env.DATABASE_URL, 123);
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
